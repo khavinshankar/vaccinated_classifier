1d24aaf43bfbd369fc0563e78b8e2f7d58261615
Khavin Shankar <khavinshankar@gmail.com>
2021-10-19 19:17:08 +0530

    replaced accuracy with metrics(accuracy, precision, recall and f1 score) in model1 and model2

--- models/model1.py
+++ models/model1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from sklearn.metrics import classification_report
 
 
 class IsVacinated(nn.Module):
@@ -15,11 +16,19 @@ class IsVacinated(nn.Module):
 
     def forward(self, x):
         return self.network(x)
-    
+
     def fit(self, x_train, y_train, x_test, y_test, epochs=60, lr=0.01, criterion=nn.BCELoss, optimizer=torch.optim.Adam):
+        accuracy_scores = []
+        precision_scores = []
+        recall_scores = []
+        f1_scores = []
+
         criterion = criterion()
         optimizer = optimizer(self.parameters(), lr=lr)
+
         x_train, y_train = self.tensorify(x_train, y_train)
+        x_test, y_test = self.tensorify(x_test, y_test)
+
         for epoch in range(1, epochs+1):
             y_pred = self(x_train)
             loss = criterion(y_pred, y_train)
@@ -29,27 +38,38 @@ class IsVacinated(nn.Module):
             optimizer.zero_grad()
 
             if epoch % 10 == 0:
-                acc = self.accuracy(x_test, y_test)
-                print(f"epoch = {epoch}, loss = {loss.item():.4f}, acc = {acc:.4f}")
-    
+                accuracy, precision, recall, f1 = self.metrics(x_test, y_test)
+                accuracy_scores.append(accuracy)
+                precision_scores.append(precision)
+                recall_scores.append(recall)
+                f1_scores.append(f1)
+
+        return {
+            "accuracy_scores": accuracy_scores,
+            "precision_scores": precision_scores,
+            "recall_scores": recall_scores,
+            "f1_scores": f1_scores
+        }
+
     def predict(self, x_test):
         x_test, _ = self.tensorify(x_test)
         with torch.no_grad():
             y_pred = self(x_test)
-        
+
         return y_pred
-      
+
     def tensorify(self, x=np.array([]), y=np.array([])):
-        x = torch.from_numpy(x.astype(np.float32))
-        y = torch.from_numpy(y.astype(np.float32))
+        x = x if torch.is_tensor(x) else torch.from_numpy(x.astype(np.float32))
+        y = y if torch.is_tensor(y) else torch.from_numpy(y.astype(np.float32))
         y = y.view(y.shape[0], 1)
-        
+
         return x, y
-    
-    def accuracy(self, x_test, y_test):
+
+    def metrics(self, x_test, y_test):
         x_test, y_test = self.tensorify(x_test, y_test)
         with torch.no_grad():
-            y_pred = self(x_test)
-            y_pred_cls = y_pred.round()
-            accuracy = y_pred_cls.eq(y_test).sum() / float(y_test.shape[0])
-            return accuracy
+            y_pred = self.predict(x_test)
+            report = classification_report(
+                y_test, y_pred.round(), output_dict=True)
+
+        return report['accuracy'], report['weighted avg']['precision'], report['weighted avg']['recall'], report['weighted avg']['f1-score']
